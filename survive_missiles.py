b7d7cbc2b889b62a34c74db7f349b8e8086a5b9a
zagoat123 <devswarup13@gmail.com>
2026-07-09 05:00:38 -0700

    Implement player movement controls
    Added keyboard controls for player movement in all directions.

--- survive_missiles.py
+++ survive_missiles.py
@@ -7,6 +7,7 @@ screen=pygame.display.set_mode((600,600))
 
 player_x=200
 player_y=200
+keys=[False,False,False,False]
 
 player=pygame.image.load("character.png")
 background=pygame.image.load("background.png")
@@ -19,4 +20,43 @@ while player_y<600:
         if event.type==QUIT:
             pygame.quit()
             exit()
-    player_y+=2
+    if event.type == pygame.KEYDOWN:
+        if event.key==K_UP:
+            keys[0]=True
+        if event.key==K_DOWN:
+            keys[1]=True
+        if event.key==K_LEFT:
+            keys[2]=True
+        if event.key==K_RIGHT:
+            keys[3]=True
+
+    
+    if event.type == pygame.KEYUP:
+        if event.key==K_UP:
+            keys[0]=False
+        if event.key==K_DOWN:
+            keys[1]=False
+        if event.key==K_LEFT:
+            keys[2]=False
+        if event.key==K_RIGHT:
+            keys[3]=False
+
+    if keys[0]:#up
+        if player_y>0:
+            player_y-=7
+
+    elif keys[1]:#down
+        if player_y<600:
+            player_y+=7
+
+    elif keys[2]:#left
+        if player_x>0:
+            player_x-=7
+
+    elif keys[3]:#right
+        if player_x<600:
+            player_x+=7
+
+    sleep(0.05)
+    player_y+=5
+
