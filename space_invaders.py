df2c188039e0008892481b5948cb315650c73a99
zagoat123 <devswarup13@gmail.com>
2026-07-16 04:47:52 -0700

    Update space_invaders.py

--- space_invaders.py
+++ space_invaders.py
@@ -18,18 +18,37 @@ FPS=60
 VEL=5
 Bullet_vel=7
 Maxbullets=3
-Spaceship_width,spaceship_height=55,40
+spaceship_width,spaceship_height=55,40
 HEALTH_FONT=pygame.font.SysFont("comicsans",40)
 WINNER_FONT=pygame.font.SysFont("comicsans",100)
 
 
-background=pygame.image.load(os.path.join("Assets","background.png"))
+background=pygame.image.load(os.path.join("assets","background.png"))
 BORDER=pygame.Rect(WIDTH//2-5,0,10,HEIGHT)
-for event in pygame.event.get():
-    if event.type==pygame.QUIT:
-        #if it is quit the pygame
-        pygame.quit()
-    exit()
+
+YELLOW_HIT=pygame.USEREVENT+1
+RED_HIT=pygame.USEREVENT+2
+
+YELLOW_SPACESHIP_IMAGE=pygame.image.load(os.path.join("assets","yellow.jpeg"))
+RED_SPACESHIP_IMAGE=pygame.image.load(os.path.join("assets","red.jpeg"))
+
+YELLOW_SPACESHIP=pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE,(spaceship_width,spaceship_height)),90)
+
+RED_SPACESHIP=pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE,(spaceship_width,spaceship_height)),-90)
+
+def main():
 
 
-print=("yooo this is dev")
+    run=True
+    while run:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                #if it is quit the pygame
+                pygame.quit()
+            exit()
+
+if __name__=="__main__":
+    main()
+
+
+   
